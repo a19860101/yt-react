37cf20596f179b5882bf1992770bdb41fb60c303
Zac <zac@ZacdeMacBook-Pro.local>
2020-07-22 15:47:18 +0800

    20200722

--- src/List.js
+++ src/List.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 class List extends Component {
     state = {
         users: ['John','Ben','Mary','Michelle','Jack'],
@@ -13,20 +15,40 @@ class List extends Component {
                 id:3,
                 name:'車輪餅'
             }
-        ]
+        ],
+        lists:[]
     }
+    componentDidMount() {
+        axios.get('https://picsum.photos/v2/list')
+        .then(
+            data => 
+            // console.log(data.data)
+            {
+                this.setState({
+                    lists: data.data
+                })
+            }
+        )
+        .catch(
+            err => console.log(err)
+        )
+    }
+    
     render() {
         return (
             <div>
                 {
-                this.state.users.map((user,idx) => <div key={idx}>{user}--{idx}</div>)
+                // this.state.users.map((user,idx) => <div key={idx}>{user}--{idx}</div>)
                 }
                 {
 
                     // this.state.users.map(function(user,idx){
                     //     return <div>{user} -- {idx}</div>
                     // })
-                    this.state.foods.map(food => <div key={food.id}>{food.name}</div>)
+                    // this.state.foods.map(food => <div key={food.id}>{food.name}</div>)
+                }
+                {
+                    this.state.lists.map(item=><div>{item.download_url}</div>)
                 }
             </div>
         );
